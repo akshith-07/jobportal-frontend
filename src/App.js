3f3074fb0fd0eaba7c046c8c9bc913774dcca93c
akshith-07 <akshith2002jobirin@gmail.com>
2023-06-26 11:30:13 +0530

    Full Implementaion  Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import GetAllJobsComponent from "./components/GetAllJobsComponent/GetAllJobsComponent";
+import AddNewJobsComponent from "./components/AddNewJobsComponent/AddNewJobsComponent";
+import EditJobsComponent from "./components/EditJobsComponent/EditJobsComponent";
+// import jobPortalImage from "./job-portal.jpg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="container">
+        {/* <img src={jobPortalImage} alt="Job Portal" className="logo" /> */}
+
+        <h1 className="head">Job Portal WebsiteApp</h1>
+        <p className="first">Find your dream job now </p>
+        <p className="second">5 lakh+ jobs for you to explore</p>
+        <nav className="nav-menu">
+          <Link to="/">Home</Link>
+          <Link to="/admin/add">Add Jobs</Link>
+          <Link to="/admin/edit">Edit Jobs</Link>
+        </nav>
+        <Routes>
+          <Route exact path="/" element={<GetAllJobsComponent />}></Route>
+          <Route path="/admin/add" element={<AddNewJobsComponent />}></Route>
+          <Route path="/admin/edit" element={<EditJobsComponent />}></Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
